7c5d65c087cf7f6aad491a86a5026fcba2258c1d
LuisDa Mendoza <luismenes027@gmail.com>
2020-06-01 18:54:17 -0500

    Agregar archivos de proyecto.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ServiciosWeb.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiciosWeb.WebAPI.WebApiApplication" Language="C#" %>
